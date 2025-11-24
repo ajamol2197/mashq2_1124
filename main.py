c333abbad0b1cf87646a4086253eab2f21aece40
ajamol2197 <ajamol2197@gmail.com>
2025-11-24 16:06:34 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,43 @@
+# 1-m
+a = input("matn kirit: ")
+
+if a in a:
+    print("topildi")
+else:
+    print("topilmadi")
+# 
+# 2-m
+a = input("matn kirit: ")
+b = input(" qidirilayotgan soz: ")
+
+if b in a:
+    print("topildi")
+else:
+    print("topilmadi")
+
+# 3-m
+a = input("matn kirit: ")
+b = input(" qidirilayotgan soz: ")
+
+if b in a:
+    print("savol bor")
+else:
+    print("savol yoq")
+
+# 4-m
+a = input("kantaqt kirit: ")
+b = input(" qidirilayotgan soz: ")
+
+if b in a:
+    print("ozb raqam")
+else:
+    print("boshqa raqam")
+# 
+# 5-m
+a = input("emile kirit: ")
+b = input(" qidirilayotgan soz: ")
+
+if b in a:
+    print("ha emile")
+else:
+    print("bashqa emile")
